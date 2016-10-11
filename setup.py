a4177bfa27d3f95db1d554d085d2ae2384c12f4a
harold <dadapapa@gmail.com>
2016-10-11 17:32:09 +0100

    setup.py etc. added

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+	name         = 'units',
+	version      = 'develop',
+	description  = 'unit aware (dimensional) quantities',
+	author       = 'Harold Fellermann',
+	author_email = 'harold.fellermann@newcastle.ac.uk',
+	license      = 'GNU General Public License v3 (GPLv3)',
+	url          = 'https://github.com/harfel/pyunits',
+	download_url = 'https://github.com/harfel/pyunits/archive/XXX.zip',
+	py_modules   = ['units'],
+)
--- units.py
+++ units.py
@@ -1,4 +1,18 @@
-"""Calculation with dimensional quantities.
+# Copyright 2016 by Harold Fellermann
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""Dimensional quantities for unit aware calculations.
 
 Quantities are values with an associated unit of a given dimension,
 such as 3.3 meters, 50 seconds, 42 meters per second.
